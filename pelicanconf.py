6a8b1788fab832786f440a1ad268774dd7c86655
yjk21 <youngjun.ko@epfl.ch>
2016-04-25 16:32:38 +0200

    added plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,3 +35,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins', './pelican-plugins']
+PLUGINS = ['ipynb.markup', 'render_math']
