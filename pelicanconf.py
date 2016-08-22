f8264d96fcf7ad821314898168fa6da801334e96
Young Jun Ko <youngjun.ko@epfl.ch>
2016-08-22 17:08:31 +0200

    convert to gh user page

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-THEME = u'pelican-simplegrey'
+THEME = u'themes/pelican-simplegrey'
 
 AUTHOR = u'yjk21'
 SITENAME = u'Untitled'
